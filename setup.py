ada55b3e4e3202c5fc4070b94a38286605854fb7
Spencer Guo <spencerchenguo@gmail.com>
2024-06-01 16:43:46 -0500

    Fixed typo in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='geom2vec',
     version='0.0.1',
-    description="Geom2Vec (geometry-to-vector) is a framework for compute vector representation of molecular conformations using pretrained graph neural networks (GNNs)."
+    description="Geom2Vec (geometry-to-vector) is a framework for compute vector representation of molecular conformations using pretrained graph neural networks (GNNs).",
     packages=find_packages(),
     install_requires=[
         'numpy',
